06ce5c810d6e11c8df71350a9713754bd9d91806
glyceryl <494192175@qq.com>
2026-05-19 01:20:05 +0800

    Initial commit

---
+++ src/main/java/com/astral_craft/AstralCraft.java
@@ -0,0 +1,16 @@
+package com.astral_craft;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.ModContainer;
+
+@Mod(AstralCraft.MOD_ID)
+public class AstralCraft {
+
+    public static final String MOD_ID = "astral_craft";
+
+    public AstralCraft(IEventBus modEventBus, ModContainer modContainer) {
+
+    }
+
+}
