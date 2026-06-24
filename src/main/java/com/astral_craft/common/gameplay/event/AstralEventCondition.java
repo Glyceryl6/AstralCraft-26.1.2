8c7fb2825e52633cf6ad4d61762566c51e8606a3
glyceryl <494192175@qq.com>
2026-06-25 05:31:06 +0800

    修复四个新增事件效果导致的 Codec 空指针。

--- src/main/java/com/astral_craft/common/gameplay/event/AstralEventCondition.java
+++ src/main/java/com/astral_craft/common/gameplay/event/AstralEventCondition.java
@@ -1,15 +1,32 @@
 package com.astral_craft.common.gameplay.event;
 
 import com.astral_craft.common.registry.AstralEventConditionTypes;
+import com.mojang.datafixers.util.Pair;
 import com.mojang.serialization.Codec;
+import com.mojang.serialization.DataResult;
+import com.mojang.serialization.DynamicOps;
 import com.mojang.serialization.MapCodec;
 
 import java.util.function.Function;
 
 public interface AstralEventCondition {
 
-    Codec<AstralEventCondition> CODEC = AstralEventConditionTypes.REGISTRY.byNameCodec()
-            .dispatch(AstralEventCondition::codec, Function.identity());
+    Codec<AstralEventCondition> CODEC = new Codec<>() {
+        @Override
+        public <T> DataResult<Pair<AstralEventCondition, T>> decode(DynamicOps<T> ops, T input) {
+            return this.delegate().decode(ops, input);
+        }
+
+        @Override
+        public <T> DataResult<T> encode(AstralEventCondition input, DynamicOps<T> ops, T prefix) {
+            return this.delegate().encode(input, ops, prefix);
+        }
+
+        private Codec<AstralEventCondition> delegate() {
+            return AstralEventConditionTypes.REGISTRY.byNameCodec()
+                    .dispatch(AstralEventCondition::codec, Function.identity());
+        }
+    };
 
     String typeId();
 
--- src/main/java/com/astral_craft/common/gameplay/event/AstralEventEffect.java
+++ src/main/java/com/astral_craft/common/gameplay/event/AstralEventEffect.java
@@ -1,7 +1,10 @@
 package com.astral_craft.common.gameplay.event;
 
 import com.astral_craft.common.registry.AstralEventEffectTypes;
+import com.mojang.datafixers.util.Pair;
 import com.mojang.serialization.Codec;
+import com.mojang.serialization.DataResult;
+import com.mojang.serialization.DynamicOps;
 import com.mojang.serialization.MapCodec;
 import net.minecraft.server.level.ServerPlayer;
 
@@ -9,8 +12,22 @@ import java.util.function.Function;
 
 public interface AstralEventEffect {
 
-    Codec<AstralEventEffect> CODEC = AstralEventEffectTypes.REGISTRY.byNameCodec()
-            .dispatch(AstralEventEffect::codec, Function.identity());
+    Codec<AstralEventEffect> CODEC = new Codec<>() {
+        @Override
+        public <T> DataResult<Pair<AstralEventEffect, T>> decode(DynamicOps<T> ops, T input) {
+            return this.delegate().decode(ops, input);
+        }
+
+        @Override
+        public <T> DataResult<T> encode(AstralEventEffect input, DynamicOps<T> ops, T prefix) {
+            return this.delegate().encode(input, ops, prefix);
+        }
+
+        private Codec<AstralEventEffect> delegate() {
+            return AstralEventEffectTypes.REGISTRY.byNameCodec()
+                    .dispatch(AstralEventEffect::codec, Function.identity());
+        }
+    };
 
     String typeId();
 
