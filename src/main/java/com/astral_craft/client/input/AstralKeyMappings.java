925064e5249a648df991df2778bac020ac575ced
glyceryl <494192175@qq.com>
2026-07-14 09:23:10 +0800

    将按键真正设置为“未设定”

--- src/main/java/com/astral_craft/client/input/AstralKeyMappings.java
+++ src/main/java/com/astral_craft/client/input/AstralKeyMappings.java
@@ -6,7 +6,6 @@ import net.minecraft.client.KeyMapping;
 import net.neoforged.neoforge.client.event.RegisterKeyMappingsEvent;
 import net.neoforged.neoforge.client.settings.KeyConflictContext;
 import net.neoforged.neoforge.common.util.Lazy;
-import org.lwjgl.glfw.GLFW;
 
 public class AstralKeyMappings {
 
@@ -14,19 +13,19 @@ public class AstralKeyMappings {
 
     public static final Lazy<KeyMapping> CARD_BACK_SELECTION = Lazy.of(() -> new KeyMapping(
             "key.astral_craft.card_back_selection", KeyConflictContext.IN_GAME,
-            InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_ESCAPE, CATEGORY));
+            InputConstants.UNKNOWN, CATEGORY));
 
     public static final Lazy<KeyMapping> CHARACTER_SETTINGS = Lazy.of(() -> new KeyMapping(
             "key.astral_craft.character_settings", KeyConflictContext.IN_GAME,
-            InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_ESCAPE, CATEGORY));
+            InputConstants.UNKNOWN, CATEGORY));
 
     public static final Lazy<KeyMapping> HAND_CARD_DECK = Lazy.of(() -> new KeyMapping(
             "key.astral_craft.hand_card_deck", KeyConflictContext.IN_GAME,
-            InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_ESCAPE, CATEGORY));
+            InputConstants.UNKNOWN, CATEGORY));
 
     public static final Lazy<KeyMapping> CHARACTER_SKILL = Lazy.of(() -> new KeyMapping(
             "key.astral_craft.character_skill", KeyConflictContext.IN_GAME,
-            InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_ESCAPE, CATEGORY));
+            InputConstants.UNKNOWN, CATEGORY));
 
     public static void register(RegisterKeyMappingsEvent event) {
         event.registerCategory(CATEGORY);
